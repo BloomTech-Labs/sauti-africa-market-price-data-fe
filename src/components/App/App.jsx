5d1c9d4ebd4634532812609b278f3f118843b353
alexisjcarr <acarr59@gmail.com>
2019-10-11 14:58:03 -0500

    📈 Adds react-ga as a dependency

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-
-import Image from "../../assets/ogImage.jpeg";
+import ReactGA from "react-ga";
+//import auth from './auth.js'; // Sample authentication provider
 
 import "./App.scss";
 
-function App() {
+const App = () => {
+  const trackingId = "UA-149875613-1"; // Replace with your Google Analytics tracking ID
+
+  ReactGA.initialize(trackingId);
+  /*ReactGA.set({
+    userId: auth.currentUserId()
+    // any data that is relevant to the user session
+    // that you would like to track with google analytics
+  });*/
+
   return (
     <div className="App">
       <p>Hello World 😎</p>
     </div>
   );
-}
+};
 
 export default App;
