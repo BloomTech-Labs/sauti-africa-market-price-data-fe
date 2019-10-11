f9504f762a2ec5589cf4542fa9400a32caed5ac2
alexisjcarr <acarr59@gmail.com>
2019-10-11 15:28:40 -0500

    ⚗ Adds stopping point for experimenting with react-ga

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,21 +4,19 @@ import ReactGA from "react-ga";
 
 import "./App.scss";
 
-const App = () => {
-  const trackingId = "UA-149875613-1"; // Replace with your Google Analytics tracking ID
+const trackingId = "UA-149875613-1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
 
-  ReactGA.initialize(trackingId);
-  /*ReactGA.set({
-    userId: auth.currentUserId()
-    // any data that is relevant to the user session
-    // that you would like to track with google analytics
-  });*/
+/*ReactGA.set({
+  userId: auth.currentUserId()
+  // any data that is relevant to the user session
+  // that you would like to track with google analytics
+});*/
 
-  return (
-    <div className="App">
-      <p>Hello World 😎</p>
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    <p>Hello World 😎</p>
+  </div>
+);
 
 export default App;
