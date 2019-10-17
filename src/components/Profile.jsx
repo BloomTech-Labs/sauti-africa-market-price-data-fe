656a965d648e6d6d229ca8d3d81c1019dbea3590
Michael Hart <michael@mhart.dev>
2019-10-17 14:47:32 -0400

    Removed firebase remnants

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 
-import Highlight from "../components/Highlight/Highlight";
-import Loading from "../components/Loading/Loading";
+import Highlight from "./Highlight/Highlight";
+import Loading from "./Loading/Loading";
 import { useAuth0 } from "../hooks/useAuth0";
 
 const Profile = () => {
