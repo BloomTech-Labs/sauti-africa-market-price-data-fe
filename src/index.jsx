4ae15beee4de500718c1f361cf839650028321ec
Michael Hart <michael@mhart.dev>
2019-10-17 17:24:25 -0400

    Disabling serviceWorkers while investigating loading error

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import * as Sentry from "@sentry/browser";
 
 import "./index.scss";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
 import { Auth0Provider } from "./hooks/useAuth0";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -40,4 +40,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
