61ae4c21ec2c1ca6c99cb029a630607b766d6b05
Miguel Peralta <eudysperalta723@gmail.com>
2019-11-01 18:03:15 -0400

    saving progress so shawn can pull
    Co-authored-by: Shawm mcManus <xsvfan76@gmail.com>

--- src/components/Landing/Carousel.js
+++ src/components/Landing/Carousel.js
@@ -1,38 +1,57 @@
-import React from 'react';
-import { UncontrolledCarousel } from 'reactstrap';
+import { CarouselProvider, Slider } from "pure-react-carousel";
+import React from "react";
+
+import CustomCardSlide from "./CardSlide";
+import CustomDotGroup from "./DotGroup";
 
 import pivot from '../../assets/Pivot-table-screenshot.png';
 import api from '../../assets/api-call-2.png';
 import { object } from 'prop-types';
 import { version } from 'moment';
 
-const items = [
-  {
-    src: pivot,
-    altText: 'Slide 1',
-    caption: 'Slide 1',
-    header: 'Slide 1 Header',
-    key: '1'
-  },
-  {
-    src: api,
-    altText: 'Slide 2',
-    caption: 'Slide 2',
-    header: 'Slide 2 Header',
-    key: '2'
-  },
-  {
-    src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-    altText: 'Slide 3',
-    caption: 'Slide 3',
-    header: 'Slide 3 Header',
-    key: '3'
-  }
-];
 
-const Example = () => <UncontrolledCarousel items={items} />;
+const CardCarousel = () => (
+  <CarouselProvider
+    naturalSlideWidth={1}
+    naturalSlideHeight={1.25}
+    totalSlides={3}
+    style={{ width: "300px" }}
+  >
+    <Slider>
+      <CustomCardSlide
+        image="https://place-hold.it/800x800&text=Matthew&fontsize=32"
+        index={0}
+        header="Matthew House"
+        meta="Friend"
+      />
+      <CustomCardSlide
+        header="Elliot Baker"
+        image="https://place-hold.it/800x800&text=Elliot&fontsize=32"
+        index={1}
+        meta="Friend"
+      />
+      <CustomCardSlide
+        header="Steve Sanders"
+        image="https://place-hold.it/800x800&text=Steve&fontsize=32"
+        index={2}
+        meta="Friend"
+      />
+    </Slider>
 
-export default Example;
+    <CustomDotGroup slides={3} />
+  </CarouselProvider>
+);
+
+export default CardCarousel;
+
+// UncontrolledCarousel.propTypes = {
+//   items: PropTypes.array.isRequired,
+//   indicators: PropTypes.bool, // default: true
+//   controls: PropTypes.bool, // default: true
+//   autoPlay: PropTypes.bool, // default: true
+// };
+
+
 
 
 
