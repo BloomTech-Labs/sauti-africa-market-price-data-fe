63a05eee00cdd6087847438ece1d78beebcc8aa4
Shawn McManus <xsvfan76@gmail.com>
2019-11-04 15:45:25 -0700

    Landing-page

--- src/components/Landing/Card.js
+++ src/components/Landing/Card.js
@@ -1,23 +1,23 @@
+import React from 'react'
+import { Card, CardText, CardBody, CardTitle, Button } from 'reactstrap'
 
-import React from 'react';
-import {
-  Card, CardText, CardBody,
-  CardTitle, Button
-} from 'reactstrap';
-
-const TopCard = (props) => {
+const TopCard = props => {
   return (
     <>
-      <Card >
-        <CardBody className='top-cardbody'>
-          <CardTitle>Card title</CardTitle>
-          <CardText>Pokem ipsum dolor sit amet Mime Jr Voltorb Tranquill Koffing Goldeen Audino. Pikachu Granbull Magmortar Bastiodon Pokemon 4Ever Missingno Fog Badge. Thundershock Earth Badge Duskull Lileep Ambipom Doduo Poliwhirl. Boulder Badge Rainbow Badge Cubone Simipour Corsola Ninetales Sigilyph. Soul Badge Solosis Luvdisc Gigalith Ice in a world we must defend Rampardos.</CardText>
+      <Card>
+        <CardBody className="top-cardbody">
+          <CardTitle>
+            Public facing API serving Sauti Africa's market price data
+          </CardTitle>
+          <CardText>
+            Up-to-date daily prices for about 100 products across 60
+            marketplaces in East Africa
+          </CardText>
         </CardBody>
         <Button className="TopCardButton">Learn More</Button>
       </Card>
-   
     </>
-  );
-};
+  )
+}
 
-export default TopCard;
+export default TopCard
--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,31 +1,24 @@
-import React from 'react';
+import React from 'react'
 
-import TopCard from './Card.js';
-import Carousel from './Carousel.js';
-import InfoBoxContainer from './InfoBoxContainer.js';
-import './Landing.scss';
+import TopCard from './Card.js'
+import InfoBoxContainer from './InfoBoxContainer.js'
+import './Landing.scss'
 
 const Landing = () => {
-
-    return (
-        <>
-            <div className="top-div-image">
-                <div className="top-div">
-                    <div className="cardDiv">
-                        <TopCard className="card"/>
-                    </div>
-                    <div className="carousel">
-                        <Carousel/>
-                    </div>
-                </div>
-            </div>
-            <div className="bot-div">
-                <InfoBoxContainer/>
-            </div>
-        </>
-
-    )
-
+  return (
+    <>
+      <div className="top-div-image">
+        <div className="top-div">
+          <div className="cardDiv">
+            <TopCard className="card" />
+          </div>
+        </div>
+      </div>
+      <div className="bot-div">
+        <InfoBoxContainer />
+      </div>
+    </>
+  )
 }
 
-export default Landing;
+export default Landing
