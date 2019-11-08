e8349252eb1385368ce010ebb0c048ca3c5ab87f
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-11-08 17:15:57 -0600

    Attempted testing with cypress
    Co-authored-by: Kathryn Rhoads <kathryn.ann.rhoads@gmail.com>

---
+++ cypress/integration/Grid.spec.js
@@ -0,0 +1,9 @@
+describe('<Grid />', () => {
+    it('grid page renders', () => {
+      cy.visit('/grid')
+      cy.get('Grid').within(() => {
+          cy.get('input:first').should('have.attr','placeholder', 'Countries')
+      })
+  
+    })
+  })
