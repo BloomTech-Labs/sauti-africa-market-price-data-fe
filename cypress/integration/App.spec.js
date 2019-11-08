b027030dc7cb97fba72e2e9af0597a22d20d370d
Adnan <adnan.chowdhury@outlook.com>
2019-11-08 16:20:31 -0600

    Tests for Doc Page

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-undef */
 describe('Client tests', () => {
   it('renders without crashing', () => {
     cy.visit('/')
-    cy.contains('Sauti Market Prices API') // delete this dummy test after we write real client code
-
+    cy.contains('Sauti')
+    // delete this dummy test after we write real client code
     /*=== Add more tests here ===*/
   })
 })
