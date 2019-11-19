bb120f774c13ffb0d469140eb7e5b6e449a37ee4
Michael Hart <michael@mhart.dev>
2019-11-19 11:31:36 -0500

    Updated tests

--- cypress/integration/alogin.spec.js
+++ cypress/integration/alogin.spec.js
@@ -22,7 +22,7 @@ describe('login', () => {
   })
   describe('<Grid />', () => {
     it('grid page renders', () => {
-      cy.visit('/grid')
+      cy.visit('/data')
       // cy.pause().reload()
       cy.get('form').within(() => {
         cy.get('div:first')
