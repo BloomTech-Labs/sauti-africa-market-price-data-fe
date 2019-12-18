7ad6e9809ae29cbac51ef18f86bf139dda0efc65
aaamg <andrewmanuelgarcia@gmail.com>
2019-12-18 17:51:13 -0500

    clickable links styled-DONE and ready for review

--- src/components/Landing/main.jsx
+++ src/components/Landing/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faServer, faBook, faTable } from '@fortawesome/free-solid-svg-icons'
+import './landing.css'
 
 const Main = () => {
   return (
