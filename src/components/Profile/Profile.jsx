38921b0ac8f023cd61507ca0d6670b1e2dc4ae7d
mxxt1 <matt@dustcloudlabs.com>
2020-02-03 12:33:24 -0500

    changing package.json script values to troubleshoot heroku deployment problem

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -41,7 +41,8 @@ const Profile = ({ apiKey, setApiKey }) => {
         },
         {
           headers: { Authorization: `Bearer ${token}` },
-          baseURL: 'https://sauti-africa-market-master.herokuapp.com/'
+          // baseURL: 'https://sauti-africa-market-master.herokuapp.com/'
+          baseURL: 'http://localhost:8888'
         }
       )
       setApiKey(response.data.key)
--- src/components/Profile/UserRoles.jsx
+++ src/components/Profile/UserRoles.jsx
@@ -8,7 +8,7 @@ const UserRoles = () => {
 
   const updateUserRole = (userId, user) => {
     axios
-      .put(`https://sauti-africa-market-master.herokuapp.com/api/users/${userId}`, user)
+      .put(`http://localhost:8888/api/users/${userId}`, user)
       .then(res => {
       })
       .catch(err => console.log(err))
