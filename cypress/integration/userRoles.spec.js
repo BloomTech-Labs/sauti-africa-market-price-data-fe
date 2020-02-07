88d90a29dc7a4c2f7825027cf3b5f03a1bcc56a7
Kayla <6lackmacaroon@gmail.com>
2020-02-06 19:57:41 -0700

    import base url

--- cypress/integration/userRoles.spec.js
+++ cypress/integration/userRoles.spec.js
@@ -1,3 +1,5 @@
+import { apiUserURL } from '../../src/urls'
+
 const userId = {
   sub: 'google-oauth2|111661795839592168840'
 }
@@ -8,7 +10,7 @@ const role = { app_metadata: {
 
 describe('Request', () => {
   it('Gets user data from the backend', () => {
-    cy.request('POST', 'https://sauti-africa-market-master.herokuapp.com/api/users', userId).should(
+    cy.request('POST', `${apiUserURL}/api/users`, userId).should(
       response => {
         expect(response.status).to.eq(200)
         expect(response).to.have.property('headers')
@@ -21,7 +23,7 @@ describe('Request', () => {
 // this test requires the auth0 sub id (returned after login) to work. The hardcoded id is currently valid, but maybe become invalid in the future.
 describe('Request', () => {
   it('Gets user data from the backend', () => {
-    cy.request('PUT', `https://sauti-africa-market-master.herokuapp.com/api/users/google-oauth2|111661795839592168840`, role).should(
+    cy.request('PUT', `${apiUserURL}/google-oauth2|111661795839592168840`, role).should(
       response => {
         expect(response.status).to.eq(200)
         expect(response).to.have.property('headers')
